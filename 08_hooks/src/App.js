d63d02f0de65a907fa44ee5171bfdec9e53f8fe1
whisoo98 <whisoo98@gmail.com>
2023-07-24 13:57:46 +0900

    useState & useEffect

--- 08_hooks/src/App.js
+++ 08_hooks/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Info from "./Info";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const [visible, setVisible] = useState(false);
+    return (
+        <div>
+            <button
+                onClick={() => {
+                    setVisible(!visible);
+                }}>
+                {visible ? "Un-visible" : "Visible"}
+            </button>
+            <hr />
+            {visible && <Info />}
+        </div>
+    );
+};
 
 export default App;
