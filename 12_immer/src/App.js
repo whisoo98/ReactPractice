f3354cf34787d682b1ba505ea0ab687be91b64f1
whisoo98 <whisoo98@gmail.com>
2023-07-25 16:28:40 +0900

    12 Immer Complete

--- 12_immer/src/App.js
+++ 12_immer/src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import { produce } from "immer";
+import { useRef, useCallback, useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const nextId = useRef(1);
+    const [form, setForm] = useState({ name: "", username: "" });
+    const [data, setData] = useState({ array: [], uselessValue: null });
+
+    const onChange = useCallback((e) => {
+        const { name, value } = e.target;
+        setForm(
+            produce((draft) => {
+                draft[name] = value;
+            })
+        );
+    }, []);
+    const onSubmit = useCallback(
+        (e) => {
+            e.preventDefault();
+            const info = {
+                id: nextId.current,
+                name: form.name,
+                username: form.username,
+            };
+            setData(
+                // ...data,
+                // array: data.array.concat(info),
+                produce((draft) => {
+                    draft.array.push(info);
+                })
+            );
+            setForm({
+                name: "",
+                username: "",
+            });
+            nextId.current += 1;
+        },
+        [form.name, form.username]
+    );
+
+    const onRemove = useCallback((id) => {
+        setData(
+            produce((draft) => {
+                draft.array.splice(
+                    draft.array.findIndex((info) => info.id === id),
+                    1
+                );
+            })
+        );
+    }, []);
+    return (
+        <div>
+            <form onSubmit={onSubmit}>
+                <input
+                    name="username"
+                    placeholder="ID"
+                    value={form.username}
+                    onChange={onChange}
+                />
+                <input
+                    name="name"
+                    placeholder="name"
+                    value={form.name}
+                    onChange={onChange}
+                />
+                <button type="submit">Submit</button>
+            </form>
+            <div>
+                <ul>
+                    {data.array.map((info) => (
+                        <li key={info.id} onClick={() => onRemove(info.id)}>
+                            {info.username} ({info.name})
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </div>
+    );
+};
 
 export default App;
